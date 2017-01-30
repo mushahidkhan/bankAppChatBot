7fd47ba32e16fa843fe68142eab5e7362b70a561
Mushahid Khan <mushahidkhan@Mushahids-MacBook-Pro.local>
2017-01-30 13:53:12 -0500

    changed the port to 5000

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ try {
 }
 
 // Webserver parameter
-const PORT = process.env.PORT || 8445;
+const PORT = process.env.PORT || 5000;
 
 // Wit.ai parameters
 const WIT_TOKEN = "DHCPUUCGWOAGZEGATVEQ4L6C6MS5YSPQ"
